8f9299c72e851c27fb2f56d4e79ad7e9f5fc98bf
Daniel Kraic <daniel.kraic@eset.sk>
2017-03-04 20:31:02 +0100

    updated index.erl

--- w2/file-index/index.erl
+++ w2/file-index/index.erl
@@ -1,5 +1,5 @@
 -module(index).
--export([get_file_contents/1,show_file_contents/1, create_index/1, format_lines/1, index_file/1, print_index/1]).
+-export([get_file_contents/1,show_file_contents/1, index_file/1]).
 
 % Used to read a file into a list of lines.
 % Example files available in:
@@ -42,7 +42,7 @@ index_file(File) ->
   Lines = get_file_contents(File),
   Index = create_index(format_lines(Lines)),
   print_index(Index).
-
+  
 
 %
 % getting file lines
@@ -52,37 +52,25 @@ index_file(File) ->
 format_lines(Lines) ->
   lists:map(fun(Line) -> format_line(Line) end, Lines).
 
-% get unique list of words in line
+% get unique list of lowercase words in line
 format_line(Line) ->
-    unique_list(remove_empty(split_line(Line))).
+  unique_list(lowercase_words(remove_short(split_line(Line)))).
 
 % split line to list of words
 split_line(Line) ->
-    string:tokens(Line, " \t\n.,!?").
+    string:tokens(Line, " \t\n.,:;!?").
 
-% remove empty strings from list
-remove_empty([]) ->
-    [];
-remove_empty([X|Xs]) ->
-    case X == "" of
-        true -> remove_empty(Xs);
-        false -> [X|remove_empty(Xs)]
-    end.
+% remove short and empty strings from list
+remove_short(Words) ->
+  lists:filter(fun(X) -> string:len(X) > 2 end, Words).
+
+% return all words to as lowercase
+lowercase_words(Words) ->
+  lists:map(fun(X) -> string:to_lower(X) end, Words).
 
 % make list of unique elements (remove duplicates)
 unique_list(List) ->
-    make_unique_list(List, []).
-
-make_unique_list([], Out) ->
-    Out;
-make_unique_list([X|Xs], Out) ->
-    case lists:member(X,Out) of
-        true ->
-            make_unique_list(Xs, Out);
-        false ->
-            make_unique_list(Xs, [X|Out])
-    end.
-
+  lists:usort(List).
 
 %
 % create index
@@ -90,10 +78,10 @@ make_unique_list([X|Xs], Out) ->
 
 % create file index (index is stored in orddict)
 create_index(Lines) ->
-    Index = orddict:new(),
-    index_lines(1, Lines, Index).
+    Index = index_lines(1, Lines, orddict:new()),
+    create_line_ranges(Index).
 
-% indexs lines
+% index lines
 index_lines(_LineNumber, [], Index) ->
     Index;
 index_lines(LineNumber, [Line|Lines], Index) ->
@@ -106,12 +94,43 @@ index_line(_LineNumber, [], Index) ->
 index_line(LineNumber, [Word|Words], Index) ->
     case orddict:find(Word, Index) of
         {ok, Value} ->
+            % word is already present in index, update index record
             UpdatedIndex = orddict:store(Word, [LineNumber|Value], Index),
             index_line(LineNumber, Words, UpdatedIndex);
         error ->
+            % word is not present in index, create new index record
             UpdatedIndex = orddict:store(Word, [LineNumber], Index),
             index_line(LineNumber, Words, UpdatedIndex)
     end.
+
+
+%
+% create ranges of lines
+%
+
+% format list of lines to ranges of lines in index
+create_line_ranges(Index) ->
+    orddict:map(fun(_Key, Value) -> create_ranges(Value) end, Index).
+
+% create ranges of lines from list of lines
+create_ranges(LineNumbers) ->
+  create_ranges(LineNumbers, []).
+
+% no lines to add to ranges, return ranges
+create_ranges([], Out) ->
+  Out;
+% no ranges was created yet, create first range
+create_ranges([H|T],[]) ->
+  create_ranges(T, [{H,H}]);
+% H item is in last range, no need to add new range
+create_ranges([H|T], [{RangeBegin,RangeEnd}|Others]) when H > RangeBegin andalso H =< RangeEnd ->
+  create_ranges(T, [{RangeBegin,RangeEnd}|Others]);
+% H item extend last range, update last range
+create_ranges([H|T], [{RangeBegin,RangeEnd}|Others]) when H > RangeBegin andalso H == RangeEnd+1 ->
+  create_ranges(T, [{RangeBegin,H}|Others]);
+% create new range from H item
+create_ranges([H|T], Others) ->
+  create_ranges(T, [{H,H}|Others]).
 
 
 %
@@ -132,20 +151,18 @@ print_index_list([H|T]) ->
 
 % print single index item
 print_index_item({Key, Value}) ->
-    io:format("{"),
-    io:format("~p", [Key]),
-    print_list(Value),
-    io:format("}~n").
+    io:format("{ ~p, ", [Key]),
+    io:format("["),
+    print_list_items(0, Value),
+    io:format("] }~n").
 
 % print list
-print_list(List) ->
-        io:format("["),
-        print_list_item(List),
-        io:format("]").
-
-print_list_item([]) ->
+print_list_items(_, []) ->
   ok;
-print_list_item([H|T]) ->
-  io:format("~p,", [H]),
-  print_list_item(T).
+print_list_items(0, [{RangeBegin,RangeEnd}|T]) ->
+  io:format("{~p,~p}", [RangeBegin,RangeEnd]),
+  print_list_items(1,T);
+print_list_items(N, [{RangeBegin,RangeEnd}|T]) ->
+  io:format(",{~p,~p}", [RangeBegin,RangeEnd]),
+  print_list_items(N+1,T).
 
